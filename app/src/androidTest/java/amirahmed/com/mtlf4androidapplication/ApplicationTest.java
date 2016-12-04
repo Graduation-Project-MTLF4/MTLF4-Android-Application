202de6b6732f6f89e0419f138c5e10857b3af81a
Amir Ahmed <amirahmedmorsy@yahoo.com>
2016-12-04 03:05:17 +0200

    Initial commit

---
+++ app/src/androidTest/java/amirahmed/com/mtlf4androidapplication/ApplicationTest.java
@@ -0,0 +1,13 @@
+package amirahmed.com.mtlf4androidapplication;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
